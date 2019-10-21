badfc828789613a429e07946917aa96ac56cf8ea
prateek-pk27 <30526910+prateek-pk27@users.noreply.github.com>
2019-10-21 23:38:30 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -5,8 +5,8 @@
   # Getting Plotting Data
   # Getting Weekday from Date
 power_consumption <- read.table("household_power_consumption.txt", header = TRUE, sep = ';')
-power_consumption <- mutate(df, Date = as.Date(Date, "%d/%m/%Y"))
-power_consumption_subset <- subset(df1, Date == "2007-02-01" | Date == "2007-02-02")
+power_consumption <- mutate(power_consumption, Date = as.Date(Date, "%d/%m/%Y"))
+power_consumption_subset <- subset(power_consumption, Date == "2007-02-01" | Date == "2007-02-02")
 global_active_power <- as.numeric(as.character(power_consumption_subset$Global_active_power))
 date_time <- strptime(paste(power_consumption_subset$Date, power_consumption_subset$Time, sep = ' '), "%Y-%m-%d %H:%M:%S")
 
